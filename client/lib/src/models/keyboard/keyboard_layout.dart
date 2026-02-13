967928cc4f05315a6a9d4f175bea4f407e73ebe0
Rafael Martínez <it-rafamartinez@outlook.es>
2026-02-13 20:53:37 +0100

    no se

--- client/lib/src/models/keyboard/keyboard_layout.dart
+++ client/lib/src/models/keyboard/keyboard_layout.dart
@@ -1,6 +1,4 @@
-
 import 'package:parkingson_key/src/models/keyboard/keyboard_item.dart';
-import 'package:shared/models/keyboard_layout_dto.dart';
 
 class KeyboardLayout {
   final String id;
@@ -13,20 +11,4 @@ class KeyboardLayout {
     required this.portrait,
     required this.landscape,
   });
-}
-
-// Mapper desde DTO (shared) a modelo de cliente
-KeyboardLayout mapFromDto(
-  KeyboardLayoutDto dto,
-  KeyboardItemRegistry registry,
-) {
-  return KeyboardLayout(
-    id: dto.id,
-    portrait: dto.portrait
-        .map((row) => row.map((id) => registry.get(id)).toList())
-        .toList(),
-    landscape: dto.landscape
-        .map((row) => row.map((id) => registry.get(id)).toList())
-        .toList(),
-  );
 }
